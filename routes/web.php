48cbbb3aaa2647fbc0fd71360ae23a75c351c28e
Armita <armitashahim9@gmail.com>
2026-09-23 14:58:15 +0330

    feat: add task crud routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,14 +9,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('guest')->group(function ()
-{
+Route::middleware('guest')->group(function () {
     Route::get('/register', function () {
         return view('auth.register');
     });
 
     Route::post('/register', [AuthController::class, 'register']);
-
 
     Route::get('/login', function () {
         return view('auth.login');
@@ -24,11 +23,16 @@ Route::middleware('guest')->group(function ()
     Route::post('/login', [AuthController::class, 'login']);
 });
 
-Route::middleware('auth')->group(function()
-{
+
+Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
+
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::get('/tasks/create', [TaskController::class, 'create']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    // Route::get('/tasks/{task}', [TaskController::class, 'show']);
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit']);
+    Route::put('/tasks/{task}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
 });
-
-
-
