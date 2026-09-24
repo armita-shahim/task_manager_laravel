789d38732ba4238f3c9c0077359c33594dbbeee0
Armita <armitashahim9@gmail.com>
2026-09-25 03:11:28 +0330

    fix: fix profile request name

--- app/Http/Requests/EditProfileRequest.php
+++ app/Http/Requests/EditProfileRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class UpdateProfileRequest extends FormRequest
+class EditProfileRequest extends FormRequest
 {
     public function authorize(): bool
     {
