b385f204a628a390d5215583e24b6c27747112ba
Armita <armitashahim9@gmail.com>
2026-09-25 03:09:16 +0330

    fix: correct migration rollback

--- database/migrations/2026_09_22_051813_create_task_user_table.php
+++ database/migrations/2026_09_22_051813_create_task_user_table.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_task');
+        Schema::dropIfExists('task_user');
     }
 };
