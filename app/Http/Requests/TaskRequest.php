fe49d4f5c63ebbc9f9e699c7c21783e603ce9dc2
Armita <armitashahim9@gmail.com>
2026-09-25 13:17:12 +0330

    feat: prevent past task due dates

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -21,7 +21,7 @@ class TaskRequest extends FormRequest
             'title' => ['required', 'string', 'max:100'],
             'description' => ['required', 'string'],
             'priority' => ['required', new Enum(Priority::class)],
-            'due_date' => ['required', 'date'],
+            'due_date' => ['required', 'date', 'after_or_equal:today'],
             'status' => ['required', new Enum(Status::class)],
             'category_id' => ['nullable', 'exists:categories,id']
         ];
